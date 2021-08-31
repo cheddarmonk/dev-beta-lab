a8772e4540d746e9ec23b80cb4f2ffa49a9777bd
cheddarmonk <44252883+cheddarmonk@users.noreply.github.com>
2021-08-31 13:42:27 -0500

    expose readme

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
+
 const express = require('express');
 const app = express();
+
 
 app.get('/', function(req, res){
 
     res.send('<h3>Hello Node on Heroku</h3>');
+
+});
+
+app.get('/about', function(req, res){
+
+    res.sendFile('/README.md');
 
 });
 

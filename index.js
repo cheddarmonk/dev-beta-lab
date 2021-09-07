6dbcc5fd6ffff39a948da9eaac12e5e4cda63e68
cheddarmonk <44252883+cheddarmonk@users.noreply.github.com>
2021-09-07 08:56:27 -0500

    change

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.get('/', function(req, res){
 
-    res.send('<h3>Hello Node on Heroku </h3> <a href="https://github.com/cheddarmonk/dev-beta-lab">GitHub</a>');
+    res.send('<h3>Hello Node on Heroku </h3>');
 
 });
 

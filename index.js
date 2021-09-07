ebe80168a620805b6964c2e72faffc2bd4457b13
cheddarmonk <44252883+cheddarmonk@users.noreply.github.com>
2021-09-07 08:58:08 -0500

    link added to index

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const app = express();
 
 app.get('/', function(req, res){
 
-    res.send('<h3>Hello Node on Heroku </h3>');
+    res.send('<h3>Hello Node on Heroku </h3> <h2>https://dev-training-beta.herokuapp.com/about</h2'
+
+    );
 
 });
 
